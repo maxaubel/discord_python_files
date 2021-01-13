69a66cb75434176c977a424a6ce7e97cbfaaba62
M Aubel <maximiliano.aubel@sansano.usm.cl>
2021-01-13 16:45:11 -0300

    Update bot-img.py
    minor changes

--- bot-img.py
+++ bot-img.py
@@ -1,6 +1,6 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
-webhook = DiscordWebhook(url='', username="LVA")
+webhook = DiscordWebhook(url='', username="HAL-9000")
 
 with open("python.png", "rb") as f:
     webhook.add_file(file=f.read(), filename='example.jpg')
