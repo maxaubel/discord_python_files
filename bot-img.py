8de85546c7128972cf1fe02d842d65f80e597641
Max Aubel <maximiliano.aubel@sansano.usm.cl>
2021-01-18 00:49:17 -0300

    test

--- bot-img.py
+++ bot-img.py
@@ -1,12 +1,10 @@
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
-<<<<<<< HEAD
 webhook = DiscordWebhook(url='YOUR URL', username="HAL-9000")
-=======
-webhook = DiscordWebhook(url='', username="HAL-9000")
->>>>>>> 69a66cb75434176c977a424a6ce7e97cbfaaba62
 
 with open("python.png", "rb") as f:
     webhook.add_file(file=f.read(), filename='example.jpg')
 
 response = webhook.execute()
+print(response)
+test
